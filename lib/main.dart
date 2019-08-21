38b5e70594094cbb18097b70db7cb1ce6d01a50a
puntari <puntari@abc.com>
2019-08-21 23:43:16 +0700

    Create Authen

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:doctor_com/screens/authen.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Authen(),
+    );
+  }
+}
