05b5b820a82eafd4dbd2969033522928dec889fe
comelydie <comelydie@gmail.com>
2020-12-22 23:47:20 +0700

    camera

--- lib/screens/camera.dart
+++ lib/screens/camera.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 class ShowCamera extends StatefulWidget {
   @override
@@ -6,10 +10,147 @@ class ShowCamera extends StatefulWidget {
 }
 
 class _ShowContactState extends State<ShowCamera> {
+  List _outputs;
+  File _image;
+  bool _loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loading = true;
+
+    loadModel().then((value) {
+      setState(() {
+        _loading = false;
+      });
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/model_unquant.tflite",
+      labels: "assets/labels.txt",
+      numThreads: 1,
+    );
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        imageMean: 0.0,
+        imageStd: 255.0,
+        numResults: 2,
+        threshold: 0.2,
+        asynch: true);
+    setState(() {
+      _loading = false;
+      _outputs = output;
+    });
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+
+  pickImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (image == null) return null;
+    setState(() {
+      _loading = true;
+      _image = image;
+    });
+    classifyImage(_image);
+  }
+
+  takePhoto() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    if (image == null) return null;
+    setState(() {
+      _loading = true;
+      _image = image;
+    });
+    classifyImage(_image);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('กล้อง'),
+    return Scaffold(
+      /*
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "โปรเจคจบก็คือต้องจบไง!",
+          style: TextStyle(color: Colors.white, fontSize: 25),
+        ),
+        backgroundColor: Colors.pink,
+        elevation: 0,
+      ),*/
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _loading
+                ? Container(
+                    height: 300,
+                    width: 300,
+                  )
+                : Container(
+                    margin: EdgeInsets.all(20),
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        _image == null ? Container() : Image.file(_image),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        _image == null
+                            ? Container()
+                            : _outputs != null
+                                ? Text(
+                                    _outputs[0]["label"],
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 20),
+                                  )
+                                : Container(child: Text(""))
+                      ],
+                    ),
+                  ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.01,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  tooltip: 'Pick Image',
+                  onPressed: pickImage,
+                  child: Icon(
+                    Icons.photo_size_select_actual_outlined,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  backgroundColor: Colors.pink,
+                ),
+                FloatingActionButton(
+                  tooltip: 'Take a photo ',
+                  onPressed: takePhoto,
+                  child: Icon(
+                    Icons.add_a_photo,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  backgroundColor: Colors.pink,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
